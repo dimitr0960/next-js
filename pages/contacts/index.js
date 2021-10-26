3b535a7f00b7bac0e3e929ce9cc0e8c85de0afe6
dmitrykovalevsky89 <dmitryk@gophotoweb.ru>
2021-10-26 19:11:06 +0300

    lesson_07

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,13 +1,48 @@
 import Heading from "../../components/Heading"
 import Head from "next/head"
+// import { useState, useEffect } from "react"
 
-const Contacts = () => (
-	<>
-		<Head>
-			<title>Contacts</title>
-		</Head>
-		<Heading text="Contacts lists"></Heading>
-	</>
-)
+export const getStaticProps = async () => {
+	const response = await fetch("https://jsonplaceholder.typicode.com/users")
+	const data = await response.json()
+
+	if (!data) {
+		return {
+			notFound: true,
+		}
+	}
+
+	return {
+		props: { contacts: data },
+	}
+}
+
+const Contacts = ({ contacts }) => {
+	// const [contacts, setContacts] = useState(null)
+
+	// -> getStaticProps()
+	// useEffect(() => {
+	// 	const fetchData = async () => {
+	// 		const response = await fetch("https://jsonplaceholder.typicode.com/users")
+	// 		const data = await response.json()
+	// 		setContacts(data)
+	// 	}
+	// 	fetchData()
+	// }, [])
+
+	return (
+		<>
+			<Head>
+				<title>Contacts</title>
+			</Head>
+			<Heading text="Contacts lists"></Heading>
+			<ul>
+				{contacts && contacts.map(({ id, name, email }) => (
+					<li key={id}><strong>{name}</strong>: {email}</li>
+				))}
+			</ul>
+		</>
+	)
+}
 
 export default Contacts;
